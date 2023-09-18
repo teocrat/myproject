04fc56bf0c7b8fc51cb087047099870a8eb68cba
teocrat <kokinsergei@gmail.com>
2023-09-18 10:12:09 +0300

    admin added

--- myproject/settings.py
+++ myproject/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'mainapp',
     'playapp',
     'gameapp',
+    'adminapp',
 ]
 
 MIDDLEWARE = [
@@ -111,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
