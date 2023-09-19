77dcfd5bb4a973936372dfcaf81c5c83128d251f
teocrat <kokinsergei@gmail.com>
2023-09-19 10:32:47 +0300

    reload

--- deployapp/admin.py
+++ deployapp/admin.py
@@ -1,3 +1,53 @@
 from django.contrib import admin
 
-# Register your models here.
+from . models import Category, Product
+
+
+@admin.action(description='Reset quantity to zero')
+def reset_quantity(modeladmin, request, queryset):
+    queryset.update(quantity=0)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'quantity']
+    ordering = ['category', '-quantity']
+    list_filter = ['date_added', 'price']
+    search_fields = ['description']
+    search_help_text = 'Search by Description field'
+    actions = [reset_quantity]
+    # fields = ['name', 'description', 'category', 'date_added', 'rating']
+    readonly_fields = ['date_added', 'rating']
+
+    fieldsets = [
+        (
+            None,
+            {
+                'classes': ['wide'],
+                'fields': ['name'],
+            }
+        ),
+        ('Подробности',
+            {
+                'classes': ['collapse'],
+                'description': 'Категория товара и его описание',
+                'fields': ['category', 'description'],
+            },
+         ),
+        (
+            'Бухгалтерия',
+            {
+                'fields': ['price', 'quantity'],
+            }
+        ),
+        (
+            'Рейтинг и прочее',
+            {
+                'description': 'Рейтинг сформирован автоматически на основе оценок покупателей',
+                'fields': ['rating', 'date_added']
+            }
+        ),
+    ]
+
+
+admin.site.register(Category)
+admin.site.register(Product, ProductAdmin)
